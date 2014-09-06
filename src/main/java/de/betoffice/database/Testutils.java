60c6de851f8bcafcb384b3d54888c12cabfa367b
winkler <andre.winkler@web.de>
2014-09-06 14:29:17 +0200

    Under construction: Database properties for test.

--- src/main/java/de/betoffice/database/Testutils.java
+++ src/main/java/de/betoffice/database/Testutils.java
@@ -5,17 +5,17 @@
  * ============================================================================
  * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
  * MODIFICATION
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
@@ -25,10 +25,11 @@ package de.betoffice.database;
 
 import de.betoffice.database.commandline.CommandLineArguments;
 import de.betoffice.database.commandline.CommandLineParser;
+import de.betoffice.database.dbload.ImportDatabase;
 
 /**
  * Command line processor.
- * 
+ *
  * @author Andre Winkler
  */
 public class Testutils {
@@ -36,8 +37,15 @@ public class Testutils {
     public static void main(String[] args) {
         CommandLineParser clp = new CommandLineParser();
         CommandLineArguments arguments = clp.parse(args, System.out);
-        
-        arguments.
+
+        switch (arguments.getCommand()) {
+        case CREATE_SCHEMA:
+        case EXPORT:
+        case IMPORT:
+            ImportDatabase.
+            default:
+                throw new IllegalStateException("Unknown testutils command!");
+        }
     }
 
 }
