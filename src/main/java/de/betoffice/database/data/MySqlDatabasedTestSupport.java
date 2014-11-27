b9c03786e08a0ab01dd7477f7ffb69166eb7159b
Andre Winkler <gluehloch@googlemail.com>
2014-11-27 20:39:56 +0100

    UPDATE: Performance measurement.

--- src/main/java/de/betoffice/database/data/MySqlDatabasedTestSupport.java
+++ src/main/java/de/betoffice/database/data/MySqlDatabasedTestSupport.java
@@ -5,17 +5,17 @@
  * ============================================================================
  * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
  * MODIFICATION
- *
+ * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
@@ -26,6 +26,10 @@ package de.betoffice.database.data;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.apache.commons.lang.time.StopWatch;
+import org.slf4j.Logger;
+
+import de.awtools.basic.LoggerFactory;
 import de.betoffice.database.test.Database;
 import de.betoffice.database.test.Masterdata;
 import de.dbload.Dbload;
@@ -63,9 +67,12 @@ public final class MySqlDatabasedTestSupport {
         }
     };
 
+    private final Logger log = LoggerFactory.make();
+
+    private StopWatch stopWatch = new StopWatch();
+
     /**
-     * Erstellt wenn nötig das Schema und spielt die Testdaten ein. Die
-     * Testdaten enthalten die Produktionsdaten der Spielzeiten 2000 - 2007.
+     * Setup the test database with data.
      *
      * @param _conn
      *            A database connection object
@@ -77,10 +84,26 @@ public final class MySqlDatabasedTestSupport {
     public void setUp(final Connection _conn, final DataLoader _dataLoader)
             throws SQLException {
 
+        stopWatch.start();
         DeleteDatabase.deleteDatabase(_conn);
+        stopWatch.stop();
+
+        if (log.isInfoEnabled()) {
+            log.info("Deleting the database: {} ms", stopWatch.getTime());
+        }
+
         if (!_dataLoader.equals(DataLoader.EMPTY)) {
+            stopWatch.reset();
+            
+            stopWatch.start();
             Dbload.read(_conn, _dataLoader.getResource());
             _conn.commit();
+            stopWatch.stop();
+            
+            if (log.isInfoEnabled()) {
+                log.info("Setup the database: {} ms", stopWatch.getTime());
+                //log.info("Setup the database: {} s", stopWatch.getTime() / 100 / 60);
+            }
         }
     }
 
